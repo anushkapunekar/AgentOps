dfab99bb5cf92cb5a60cc2419efd1f582fefdd1a
anushkapune01@gmail.com <160164659+anushkapunekar@users.noreply.github.com>
2025-12-04 19:44:04 +0530

    fix:main.py for connection failure

--- main.py
+++ main.py
@@ -32,7 +32,10 @@ app = FastAPI(
 
 origins = [
     "http://localhost:3000",
+    "https://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://agentops-2wff.onrender.com",
+    "https://www.agentops-2wff.onrender.com",
     "*"
 ]
 
